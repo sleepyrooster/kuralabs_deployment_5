564a1cd20392a0e983722f7b81cde97b036ae7ab
Walter Emmanuel <53017792+sleepyrooster@users.noreply.github.com>
2022-11-17 01:44:38 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,17 @@
-from application import app, greet
+from application import app
 
-def test_quick():
-  a = "jeff"
-  greeting = greet(a)
-  assert greeting == "Hi jeff "
+def test_home_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    
+def test_URL_Page():
+    response = app.test_client().get('/your-url')
+    assert response.status_code == 302
+    
+def test_error_page():
+    response = app.test_client().get('/page_not_found')
+    assert response.status_code == 404
 
-# def test_home_page():
-#     response = app.test_client().get('/')
-#     assert response.status_code == 200
+def test_error_page():
+    response = app.test_client().get('/x')
+    assert response.status_code == 404
